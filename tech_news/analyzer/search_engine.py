930a7b0a87324556227abba3e47bdb59e828a48d
fabricioFalcao <fabriciohfd@gmail.com>
2024-06-25 16:57:22 -0300

    Requisito 8

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 7
@@ -9,8 +10,13 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        timestamp = datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    found_news = search_news({"timestamp": timestamp})
+    return [(news["title"], news["url"]) for news in found_news]
 
 
 # Requisito 9
