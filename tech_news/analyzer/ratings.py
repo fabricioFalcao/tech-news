224af0adf16c463ed64dfb89035137368cd350a0
fabricioFalcao <fabriciohfd@gmail.com>
2024-06-25 17:21:56 -0300

    Requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,15 @@
+from tech_news.database import get_collection
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    collection = get_collection()
+    pipeline = [
+        {"$unwind": "$category"},
+        {"$group": {"_id": "$category", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1, "_id": 1}},
+        {"$limit": 5},
+    ]
+    categories = list(collection.aggregate(pipeline))
+    top_5_categories = [category["_id"] for category in categories]
+    return top_5_categories
